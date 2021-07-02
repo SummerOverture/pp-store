99217a3acb78ec3d330520fd69ab5edafa78133a
qiancheng <qiancheng06@corp.netease.com>
2021-07-02 11:39:33 +0800

    feat：解决循环依赖问题

--- src/inject.js
+++ src/inject.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
-import ppStore from './index';
 
 const defaultOptions = {
   forwardRef: false,
   pure: false,
 };
 
-export function inject(mapStore, options = {}) {
+const inject = function (ppStore, mapStore, options = {}) {
   const config = {
     ...defaultOptions,
-    ...options
+    ...options,
   };
 
   return WrappedComp => {
     const {
-      getDisplayName = (name) => `Inject(${name})`,
+      getDisplayName = name => `Inject(${name})`,
       pure,
       forwardRef,
     } = config;
@@ -24,12 +23,13 @@ export function inject(mapStore, options = {}) {
       const injectProps = mapStore(ppStore.stores);
       const newProps = { ...injectProps, ...props };
 
-      return <WrappedComp {...newProps} ref={ppStoreForwardedRef}/>;
+      return <WrappedComp {...newProps} ref={ppStoreForwardedRef} />;
     }
 
     const Inject = pure ? React.memo(InjectFunction) : InjectFunction;
 
-    const wrappedComponentName = WrappedComp.displayName || WrappedComp.name || 'Component';
+    const wrappedComponentName =
+      WrappedComp.displayName || WrappedComp.name || 'Component';
     const displayName = getDisplayName(wrappedComponentName);
     Inject.displayName = InjectFunction.displayName = displayName;
     Inject.WrappedComponent = WrappedComp;
@@ -46,5 +46,11 @@ export function inject(mapStore, options = {}) {
     }
 
     return hoistStatics(Inject, WrappedComp);
-  }
-}
+  };
+};
+
+export const createInject = function (ppStore) {
+  return function (mapStore, options) {
+    return inject(ppStore, mapStore, options);
+  };
+};
